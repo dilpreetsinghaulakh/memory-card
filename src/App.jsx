9ab9a363bab23c8981afb9f48ab1daa6bd50d193
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2024-01-09 21:45:02 +0530

    Add header and main content to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import icon from "/icon.svg";
+import "@fontsource/syncopate";
+import "@fontsource/bangers";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header className="navbar">
+        <a href="/" alt="Icon with home link">
+          <img src={icon} alt="icon" /> Memory Game
+        </a>
+      </header>
+
+      <main className="main-content">
+        <div className="background">
+          <div className="sun"></div>
+          <div className="ground"></div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
