7216b9a7559f8b2cbd03a0f642818e4ecf000a53
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2024-01-15 23:00:24 +0530

    Refactor game wrapper scaling and card layout

--- src/components/GameWrapper.jsx
+++ src/components/GameWrapper.jsx
@@ -5,15 +5,47 @@ import { carImageData } from "../api/imageData";
 import getImages from "../api/getImages";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useRef } from "react";
 
 export default function GameWrapper() {
+  const gameWrapper = useRef(null);
+
+  useEffect(() => {
+    if (window.innerWidth < 1280) {
+      const windowWidth = window.innerWidth;
+      const wrapperWidth = gameWrapper.current.offsetWidth;
+
+      console.log(windowWidth / wrapperWidth);
+
+      gameWrapper.current.style.transform = `scale(${
+        (windowWidth * 0.8) / 1000
+      })`;
+    }
+
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < 1280) {
+        const windowWidth = window.innerWidth;
+        const wrapperWidth = gameWrapper.current.offsetWidth;
+
+        console.log(windowWidth / wrapperWidth);
+
+        gameWrapper.current.style.transform = `scale(${
+          (windowWidth * 0.8) / 1000
+        })`;
+      }
+      else {
+        gameWrapper.current.style.transform = `scale(1)`;
+      }
+    });
+  }, []);
+
   return (
     <main className="game-main-content">
       <div className="game-wrapper-background">
         <div className="game-wrapper-sun"></div>
         <div className="game-wrapper-ground"></div>
       </div>
-      <div className="game-wrapper">
+      <div className="game-wrapper" ref={gameWrapper}>
         {gameLogic(null, 5).choices.map((id) => {
           const [data, setPhotosResponse] = useState(null);
           return (
