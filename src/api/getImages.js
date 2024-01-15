e6a28653f2bdd849f10d104d2fa8ad99337af9ab
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2024-01-15 22:09:04 +0530

    Remove dotenv dependency and update access key in getImages.js

--- src/api/getImages.js
+++ src/api/getImages.js
@@ -1,10 +1,7 @@
 import { createApi } from "unsplash-js";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const api = createApi({
-  accessKey: process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY,
+  accessKey: import.meta.env.VITE_UNSPLASH_API_ACCESS_KEY,
 });
 
 export default function getImages(imageId) {
