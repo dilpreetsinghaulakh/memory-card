e2c3a375f84d11a9803c0216915c70cebf5929de
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2024-01-25 23:16:40 +0530

    Refactor gameLogic.js and GameWrapper.jsx

--- src/api/gameLogic.js
+++ src/api/gameLogic.js
@@ -1,57 +1,81 @@
+import { useState } from "react";
 import { carImageData } from "./imageData";
 
-let prevChoiceArray = [];
-let score = 0;
-let highScore = JSON.parse(localStorage.getItem("highScore")) || 0;
+export default function newGame(numberOfChoices) {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(
+    JSON.parse(localStorage.getItem("highScore")) || 0
+  );
+  const [choices, setChoices] = useState(getNewChoices([], numberOfChoices));
+  const [prevChoiceArray, setPrevChoiceArray] = useState(new Array());
 
-export default function gameLogic(choice, numberOfChoices) {
-  if (choice === null) {
-    prevChoiceArray = [];
-    score = 0;
-    return {
-      score: score,
-      highScore: highScore,
-      choices: getNewChoices([], numberOfChoices),
-    };
-  } else {
-    if (!prevChoiceArray.includes(choice)) {
-      score++;
-      if (score > highScore) {
-        highScore = score;
-        localStorage.setItem("highScore", JSON.stringify(highScore));
-      }
-      prevChoiceArray.push(choice);
-      return {
-        score: score,
-        highScore: highScore,
-        choices: getNewChoices([], numberOfChoices),
-      };
+  return {
+    score: score,
+    highScore: highScore,
+    choices: choices,
+    choiceFunction: choiceFunction,
+  };
+
+  function choiceFunction(choice) {
+    if (
+      !prevChoiceArray.includes(choice) &&
+      prevChoiceArray.length < carImageData.length - 5
+    ) {
+      let newPrevChoiceArray = prevChoiceArray;
+      newPrevChoiceArray.push(choice);
+      setPrevChoiceArray(newPrevChoiceArray);
+
+      setScore(newPrevChoiceArray.length);
+
+      setHighScore(score + 1 > highScore ? score + 1 : highScore);
+
+      localStorage.setItem(
+        "highScore",
+        JSON.stringify(score + 1 > highScore ? score + 1 : highScore)
+      );
+
+      setChoices(getNewChoices(prevChoiceArray, numberOfChoices));
     } else {
-      return {
-        score: score,
-        highScore: highScore,
-        choices: null,
-      };
+      // setScore(0);
+      // setPrevChoiceArray([]);
+      // setChoices(getNewChoices([], numberOfChoices));
+      console.log("game over");
     }
   }
 }
 
 function getNewChoices(prevChoiceArray, numberOfChoices) {
-  let availableChoices = prevChoiceArray.length
+  let availableChoices = !prevChoiceArray.length
     ? Array.from(Array(carImageData.length).keys())
     : Array.from(Array(carImageData.length).keys()).filter(
         (item) => !prevChoiceArray.includes(item)
       );
 
-  if (availableChoices.length) {
-    let choiceArray = [];
-
-    for (let i = 0; i < numberOfChoices; i++) {
-      let randomIndex = Math.floor(Math.random() * availableChoices.length);
-      choiceArray.push(availableChoices[randomIndex]);
-      availableChoices.splice(randomIndex, 1);
+  function getRandomArray(length) {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
     }
 
-    return choiceArray;
-  } else return null;
+    let array = [];
+    for (let i = 0; i < length; i++) {
+      let randomNum = getRandomInt(carImageData.length);
+      while (array.includes(randomNum)) {
+        randomNum = getRandomInt(carImageData.length);
+      }
+      array.push(randomNum);
+    }
+    return array;
+  }
+
+  let choiceArray;
+
+  do {
+    choiceArray = getRandomArray(numberOfChoices);
+  } while (
+    availableChoices &&
+    choiceArray.filter((item) => !availableChoices.includes(item)).length ===
+      numberOfChoices
+  );
+
+  return choiceArray;
 }
--- src/components/GameWrapper.jsx
+++ src/components/GameWrapper.jsx
@@ -1,23 +1,20 @@
 import Card from "./Card";
 import "./styles/gameWrapper.css";
-import gameLogic from "../api/gameLogic";
 import { carImageData } from "../api/imageData";
 import getImages from "../api/getImages";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
+import newGame from "../api/gameLogic";
 
 export default function GameWrapper() {
-  const gameWrapper = useRef(null);
+  const cardWrapper = useRef(null);
 
   useEffect(() => {
     if (window.innerWidth < 1280) {
       const windowWidth = window.innerWidth;
-      const wrapperWidth = gameWrapper.current.offsetWidth;
 
-      console.log(windowWidth / wrapperWidth);
-
-      gameWrapper.current.style.transform = `scale(${
+      cardWrapper.current.style.transform = `scale(${
         (windowWidth * 0.8) / 1000
       })`;
     }
@@ -25,19 +22,17 @@ export default function GameWrapper() {
     window.addEventListener("resize", () => {
       if (window.innerWidth < 1280) {
         const windowWidth = window.innerWidth;
-        const wrapperWidth = gameWrapper.current.offsetWidth;
 
-        console.log(windowWidth / wrapperWidth);
-
-        gameWrapper.current.style.transform = `scale(${
+        cardWrapper.current.style.transform = `scale(${
           (windowWidth * 0.8) / 1000
         })`;
-      }
-      else {
-        gameWrapper.current.style.transform = `scale(1)`;
+      } else {
+        cardWrapper.current.style.transform = `scale(1)`;
       }
     });
   }, []);
+
+  const game = newGame(5);
 
   return (
     <main className="game-main-content">
@@ -45,30 +40,33 @@ export default function GameWrapper() {
         <div className="game-wrapper-sun"></div>
         <div className="game-wrapper-ground"></div>
       </div>
-      <div className="game-wrapper" ref={gameWrapper}>
-        {gameLogic(null, 5).choices.map((id) => {
-          const [data, setPhotosResponse] = useState(null);
-          return (
-            useEffect(() => {
-              getImages(Object.values(carImageData[id])[0])
-                .then((result) => {
-                  setPhotosResponse(result.response.urls.small);
-                })
-                .catch((error) => {
-                  console.log(error);
-                });
+      <div className="game-wrapper">
+        <div className="game-score">
+          <p>Hi {game.highScore}</p>
+          <p>Score {game.score}</p>
+        </div>
 
-              return () => {};
-            }, []),
-            (
+        <div className="card-wrapper" ref={cardWrapper}>
+          {game.choices.map((id) => {
+            const [data, setPhotosResponse] = useState(null);
+            getImages(Object.values(carImageData[id])[0])
+              .then((result) => {
+                setPhotosResponse(result.response.urls.small);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+            return (
               <Card
                 key={id}
                 image={data}
-                // clicked={card.clicked}
+                handleClick={() => {
+                  game.choiceFunction(id);
+                }}
               />
-            )
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </main>
   );
