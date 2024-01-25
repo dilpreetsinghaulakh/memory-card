11ff1be9f038086a0bbed14e3aa5ae45a99bcd46
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2024-01-25 23:16:32 +0530

    Add click event to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,18 @@
+import { useEffect, useRef } from "react";
 import "./styles/card.css";
 
 export default function Card(props) {
+  const card = useRef(null);
+
+  useEffect(() => {
+    card.current.addEventListener("click", () => {
+      props.handleClick();
+    });
+  }, []);
+
   return (
-    <div className="card">
-      <img
-        className="card-image"
-        src={props.image}
-        alt={props.name}
-        // onClick={() => props.handleClick(props.id)}
-      />
-      {/* <p>{props.name}</p> */}
+    <div className="card" ref={card}>
+      <img className="card-image" src={props.image} alt={props.name} />
     </div>
   );
 }
